312128c5413db7088934ea24f544c5c7ecc1ea35
InoaPSilva <inoaliberato@gmail.com>
2021-11-22 13:07:10 -0300

    Fix: Found image upload problem

--- src/app/features/projects/pages/edit-project/edit-project.component.ts
+++ src/app/features/projects/pages/edit-project/edit-project.component.ts
@@ -9,45 +9,78 @@ import { ProjectService } from '../../service/project.service';
   styleUrls: ['./edit-project.component.css']
 })
 
+
+
 export class EditProjectComponent implements OnInit {
+  files:any = []
+  form = this.fb.group({
+    title: [''],
+    summary: [''],
+    objective: [''],
+    category: [''],
+    files: [''],
+    
+  });
 
   constructor(private fb: FormBuilder,
     private route: ActivatedRoute,
     private project: ProjectService) { }
 
 
+  onChange(event: any) {
+    const a =  [event.target.files[0]];
+    this.files.push(a)
+    console.log(this.files);
+      
+
+  }
+
+
   editProject() {
     
-
-
+    const form = new FormData();
     
-    // return this.project.edit(
-    //   this.route.snapshot.paramMap.get('id'),
-    //   this.data.get("title"),
-    //   this.data.get("summary"),
-    //   this.data.get("objective"),
-    //   this.data.get("category"),    
-    //   this.data.get("files")
-    //   ).subscribe(
-    //   (response) => {
-    //     console.log("func inside");
+    form.append('icon', this.files[0][0]);
+    form.append('banner', this.files[1][0]);
 
-    //     if (response !== undefined) {
-    //       console.log(response);
+    const files = {'files': [ form.get("icon"), form.get("banner") ]}
 
-    //       console.log("insira mensagem de sucesso");
+    let a:any = [
+      this.form.get("title")?.value,
+      this.form.get("summary")?.value,
+      this.form.get("objective")?.value,
+      this.form.get("category")?.value,
+      files
+    ]
+    console.log(a);
+    
+    return this.project.edit(
+      this.route.snapshot.paramMap.get('id'),
+      this.form.get("title"),
+      this.form.get("summary"),
+      this.form.get("objective"),
+      this.form.get("category"),
+      form.get("files")
+    ).subscribe(
+      (response) => {
+        console.log("func inside");
 
-    //     }
-    //   },
-    //   (error) => {
+        if (response !== undefined) {
+          console.log(response);
 
-    //     if (error.error !== undefined) {
-    //       console.log(error.error.text);
+          console.log("insira mensagem de sucesso");
 
-    //       console.log("insira mensagem de erro");
+        }
+      },
+      (error) => {
 
-    //     }
-    //   })
+        if (error.error !== undefined) {
+          console.log(error.error.text);
+
+          console.log("insira mensagem de erro");
+
+        }
+      })
 
   }
 
